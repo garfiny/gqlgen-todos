23f8a0955a08ca9f9e8dd50c30e4b3832a1ef206
garfiny <shuo.zhao.mx@gmail.com>
2024-09-30 14:12:25 +1000

    added volume for postgres

---
+++ postgres/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, email) VALUES ('test1', 'test1@gmail.com')
+INSERT INTO users (username, email) VALUES ('test2', 'test2@gmail.com')
+INSERT INTO users (username, email) VALUES ('test3', 'test3@gmail.com')
